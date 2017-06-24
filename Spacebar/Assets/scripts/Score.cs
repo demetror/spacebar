0709f70618994fc82b9192aa09599361e1e334a8
gabriel <gabriel.tryjefaczka@epitech.eu>
2017-06-24 13:50:50 +0200

    score aded

---
+++ Spacebar/Assets/scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public GUIText text;
+    public int score;
+
+    private void Start()
+    {
+        score = 0;
+        UpdateScore()
+    }
+
+    public void  incScore(int add) {
+        score = score + add;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        text.text = "Score :" + score;
+    }
+
+}
