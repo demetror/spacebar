cb1e00eed36ed17bc61f21c09f0c3788698bdfeb
Soraaz <robin1.partouche@epitech.eu>
2017-06-24 05:15:26 +0200

    test

--- Spacebar/Assets/scripts/Enemy.cs
+++ Spacebar/Assets/scripts/Enemy.cs
@@ -2,7 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
+    private bool hasSpawn;
+    private Move moveScript;
+    private WeaponShoot[] weapons;
 
+    void Awake()
+    {
+        // Récupération de toutes les armes de l'ennemi
+        weapons = GetComponentsInChildren<WeaponShoot>();
 
+        // Récupération du script de mouvement lié
+        moveScript = GetComponent<Move>();
+    }
+
+    // 1 - Disable everything
+    void Start()
+    {
+        hasSpawn = false;
+
+        // On désactive tout
+        // -- collider
+        GetComponent<Collider2D>().enabled = false;
+        // -- Mouvement
+        moveScript.enabled = false;
+        // -- Tir
+        foreach (WeaponShoot weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        // 2 - On vérifie si l'ennemi est apparu à l'écran
+        if (hasSpawn == false)
+        {
+            if (GetComponent<Renderer>().IsVisibleFrom(Camera.main))
+            {
+                Spawn();
+            }
+        }
+        else
+        {
+            // On fait tirer toutes les armes automatiquement
+            foreach (WeaponShoot weapon in weapons)
+            {
+                if (weapon != null && weapon.enabled && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+
+            // 4 - L'ennemi n'a pas été détruit, il faut faire le ménage
+            if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // 3 - Activation
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        // On active tout
+        // -- Collider
+        GetComponent<Collider2D>().enabled = true;
+        // -- Mouvement
+        moveScript.enabled = true;
+        // -- Tir
+        foreach (WeaponShoot weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
--- Spacebar/Assets/scripts/Player.cs
+++ Spacebar/Assets/scripts/Player.cs
@@ -12,9 +12,9 @@ public class Player : MonoBehaviour {
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
@@ -34,7 +34,29 @@ public class Player : MonoBehaviour {
                 weapon.Attack(false);
             }
         }
+        var dist = (transform.position - Camera.main.transform.position).z;
 
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z
+          );
 
     }
 
